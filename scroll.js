9eb9ce55cef62e8175dba5312cf7f146c20fd85b
star-scope <caleb.faulkner@icloud.com>
2020-09-09 17:03:05 -0400

    commit dis b

--- scroll.js
+++ scroll.js
@@ -1,27 +1,26 @@
 var elem = document.getElementById("idcontent");
 var elem2 = document.getElementById("hoodHub");
+var elem3 = document.getElementById("idcontent");
 var distance = 0;
 var distance2 = elem2.clientHeight;
-console.log(distance2);
 
 do {
     distance += elem.offsetTop;
     distance2 += elem2.offsetTop;
-    console.log(distance2);
     elem = elem.offsetParent;
     elem2 = elem2.offsetParent;
 } while(elem && elem2);
 
-
+var distance3 = distance + elem3.clientHeight;
 var windw = this;
 
-$.fn.followTo = function ( pos ) {
+$.fn.followTo = function (pos) {
     var $this = this,
         $window = $(windw);
     
-    $window.scroll(function(e){
-        var spacer = pos + 200;
-        if ($window.scrollTop() > pos) {
+    $window.scroll(function(){
+        var spacer = distance2 - elem3.clientHeight;
+        if (($window.scrollTop() + distance3) > pos) {
             $this.css({
                 position: 'absolute',
                 top: spacer
@@ -31,9 +30,14 @@ $.fn.followTo = function ( pos ) {
                 position: 'fixed',
                 top: distance
             });
-            console.log(distance);
         }
     });
 };
 
 $('#idcontent').followTo(distance2);
+
+window.addEventListener('resize', function(){
+    if(window.innerWidth > 1200){
+        $('#idcontent').followTo(distance2);
+    }
+});
